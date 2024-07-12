e609e50e1abc47d1ae066fbf340857be8521975a
Sergey <loveykosergey@ukr.net>
2024-07-12 15:29:16 +0300

    feat: lesson 5, async actions -> connected tmdb, implement search movie

--- src/App.vue
+++ src/App.vue
@@ -34,12 +34,15 @@
         :movie="movie"
       />
     </div>
-    <div class="search" v-else>Search</div>
+    <div class="search" v-else>
+      <Search />
+    </div>
   </main>
 </template>
 
 <script setup>
 import Movie from './components/Movie.vue'
+import Search from './components/Search.vue'
 import { useMovieStore } from './stores/MovieStore'
 
 const setTab = (id) => {
---
+++ src/stores/SearchStore.js
@@ -0,0 +1,20 @@
+import { defineStore } from "pinia"
+// const url = 'https://api.themoviedb.org/3/search/movie/11?api_key=a1149a39e1c8d1209955cce47bc56e01='
+const urlSearch = 'https://api.themoviedb.org/3/search/movie?query='
+const key = '&api_key=a1149a39e1c8d1209955cce47bc56e01'
+
+export const useSearchStore = defineStore('searchStore', {
+  state: () => ({
+    loader: false,
+    movies: []
+  }),
+  actions: {
+    async getMovies(search) {
+      this.loader = true
+      const res = await fetch(`${urlSearch}${search}${key}`)
+      const data = await res.json()
+      this.movies = data.results
+      this.loader = false
+    }
+  }
+})
